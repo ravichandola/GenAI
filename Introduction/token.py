e8a085a1c8af137a09384ec151a4b8839c204237
Ravi Chandola <ravichandola94@gmail.com>
2025-05-31 15:32:36 +0530

    Build weather agent

--- Introduction/token.py
+++
@@ -1,7 +0,0 @@
-enc = tiktoken.get_encoding("cl100k_base")  # Updated to use the correct method
-text = "Hello, world!"
-tokens = enc.encode(text)
-print("Tokens:", tokens)
-
-detokenized_text = enc.decode(tokens)
-print("Detokenized text:", detokenized_text)
